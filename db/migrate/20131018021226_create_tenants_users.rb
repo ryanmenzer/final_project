36d9dfbbe1b56da1f9ef5346059608dcc53d0eae
Eigil Sagafos <eigil@sagafos.no>
2013-10-18 14:23:56 -0500

    fix some error in migrations for milia to work

--- db/migrate/20131018021226_create_tenants_users.rb
+++ db/migrate/20131018021226_create_tenants_users.rb
@@ -1,8 +1,8 @@
 class CreateTenantsUsers < ActiveRecord::Migration
   def change
     create_table :tenants_users, id: false do |t|
-      t.integer :user_id
       t.integer :tenant_id
+      t.integer :user_id
     end
 
     add_index :tenants_users, :tenant_id
