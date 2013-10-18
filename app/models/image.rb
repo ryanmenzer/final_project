b79d15d4165e7985a6c4b06362c400ec7f12f661
Ryan Menzer <ryan.c.menzer@gmail.com>
2013-10-18 18:09:59 -0500

    set some config settings

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,13 @@
 class Image < ActiveRecord::Base
 
+	attr_accessible :something, #column name where image goes
+								:remote_image_url 
+
   acts_as_tenant
 
   # belongs_to :tenant
   belongs_to :imageable, polymorphic: true
+
+  mount_uploader :image, ImageUploader
 
 end
