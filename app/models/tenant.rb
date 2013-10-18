777768415e2967e8e70aaddf5550934933a24c23
Eigil Sagafos <eigil@sagafos.no>
2013-10-18 12:15:07 -0500

    fixed realtions in tenant model

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,4 +2,7 @@ class Tenant < ActiveRecord::Base
 
   #acts_as_universal_and_determines_tenant
 
+  has_and_belongs_to_many :users
+  belongs_to             :primary_contact, class_name: "User"
+
 end
