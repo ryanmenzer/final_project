7a01d985fbe0197098dbffa09c8af5202e16729c
Eigil Sagafos <eigil@sagafos.no>
2013-10-18 12:16:12 -0500

    removed tenant_id null:false from all migrations

--- db/migrate/20131018021938_create_categories.rb
+++ db/migrate/20131018021938_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.integer :tenant_id, null: false
+      t.integer :tenant_id
       t.string :name,       null: false
 
       t.timestamps
