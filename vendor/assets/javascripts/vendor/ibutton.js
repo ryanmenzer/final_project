55793bcd77120499733c2c3e9c22e64b4a339563
Maria Fischinger <mmfischinger@gmail.com>
2013-10-18 13:23:55 -0500

    Move files into vendor assets

---
+++ vendor/assets/javascripts/vendor/ibutton.js
@@ -0,0 +1,3 @@
+//= require vendor/ibutton/jquery.easing.1.3
+//= require vendor/ibutton/jquery.metadata
+//= require vendor/ibutton/jquery.ibutton
---
+++ vendor/assets/javascripts/vendor/justgage.js
@@ -0,0 +1,2 @@
+//= require vendor/justgage/raphael.2.1.0.min
+//= require vendor/justgage/justgage.1.0.1
