7baa369c44b34d41ebf17375f11119041fea072a
Ryan Menzer <ryan.c.menzer@gmail.com>
2013-10-20 14:14:00 -0500

    initiatives model, view, controller

---
+++ app/controllers/initiatives_controller.rb
@@ -0,0 +1,42 @@
+class InitiativesController < ApplicationController
+
+	def index
+		@initiatives = Initiative.all
+	end
+
+	def new
+		@initiative = Initiative.new
+	end
+
+	def create
+		initiative = Initiative.new(params[:initiative])
+		initiative.initiative_type_id = 0
+		initiative.save!
+		redirect_to action: "index"
+	end
+
+	def show
+		@initiative = Initiative.find(params[:id])
+	end
+
+	def edit
+		@initiative = Initiative.find(params[:id])
+	end
+
+	def update
+		@initiative = Initiative.find(params[:id])
+
+		if @initiative.update_attributes(params[:initiative])
+			redirect_to @initiative
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@initiative = Initiative.find(params[:id])
+		@initiative.destroy
+		redirect_to action: "index"
+	end
+
+end
