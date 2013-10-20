a3579e69c068fa13873945ddafa41feba3fc7f3e
Eigil Sagafos <eigil@sagafos.no>
2013-10-19 19:29:30 -0500

    clean up in homes and images

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,9 @@
 class HomesController < ApplicationController
+
   skip_before_filter :authenticate_user!, :only => [ :index ]
 
   def index
     @people = Person.all
   end
+
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,8 @@
 class ImagesController < ApplicationController
 
 	def index
-		@image = Image.last
-		# @images = Image.all
+		# @image = Image.last
+		@images = Image.all
 	end
 
 	def new
@@ -11,26 +11,7 @@ class ImagesController < ApplicationController
 
 	def create
 		@image = Image.create(params[:image])
-		redirect_to :index
-		# render @image
-
-		# uploader = ImageUploader.new
-		# uploader.store!(params[:image])
-		# @image = uploader.retrieve_from_store!(params[:image])
-		# puts "=============="
-		# puts uploader
-		# puts "=============="
-
-		# render :index
-
-		# @image = Image.new(:title => "test", :url => "http://www.test.com")
-		# @image.save
-		# redirect_to "new"
-		# # if @image.save
-		# # 	render "index"
-		# # else
-		# # 	render 'new'
-		# # end
+		render :index
 	end
 
 end
