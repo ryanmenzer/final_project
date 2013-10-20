23e752f847b25fe635483442e804bcbc85c34c16
Maria Fischinger <mmfischinger@gmail.com>
2013-10-19 21:39:23 -0500

    Work on delete person

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAICZOFKP7CLQL7O7A',                        # required
-    :aws_secret_access_key  => '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'                       # required
+    :aws_access_key_id      => ENV['aws_access_key_id'],                     # required
+    :aws_secret_access_key  => ENV['aws_secret_access_key']                   # required
     # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     # :host                   => 's3.example.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
