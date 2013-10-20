c95a6d40eca04d1cb213b1d30cddec45ef79d781
Maria Fischinger <mmfischinger@gmail.com>
2013-10-19 22:46:25 -0500

    Save changes to merge recent changes

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,12 @@
 class PeopleController < ApplicationController
 
-<<<<<<< HEAD
-	def index
-		@people = Person.all
-	end
+  def index
+    if params[:cat]
+      @people = Person.where(category_id: params[:cat])
+    else
+      @people = Person.all
+    end
+  end
 
 	def new
 		@person = Person.new
@@ -33,15 +36,13 @@ class PeopleController < ApplicationController
 
 	def show
 		@person = Person.find(params[:id])
-		cid = @person.category_id
-		@category = Category.find(cid)
 	end
 
   def destroy
-    p person = Person.find(params[:id])
+    person = Person.find(params[:id])
     person.destroy
 
-    render 'index'
+    redirect_to 'people'
   end
 
 end
@@ -52,20 +53,6 @@ end
              #      person GET    /people/:id(.:format)          people#show
              #             PUT    /people/:id(.:format)          people#update
              #             DELETE /people/:id(.:format)          people#destroy
-=======
-  def index
-    if params[:cat]
-      @people = Person.where["category_id = 1"]
-    else
-      @people = Person.where["category_id IS 1"]
-    end
-  end
 
-  def new
-  end
 
-  def create
-  end
 
-end
->>>>>>> master
