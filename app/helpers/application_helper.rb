f7efdb3bb1c54e46bded1a7b4013e6f33a0a5552
Eigil Sagafos <eigil@sagafos.no>
2013-10-21 10:57:26 -0500

    emails now a universal helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def send_email(object)
+    puts "==============================="
+    puts "==============================="
+    puts "==============================="
+    puts object
+    puts "==============================="
+  end
+
+
 end
