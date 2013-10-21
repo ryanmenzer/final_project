722112f9cc544622b09180c31f15b817d089c3b8
Eigil Sagafos <eigil@sagafos.no>
2013-10-21 10:54:19 -0500

    added stories controller and views folder

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,26 @@
+class StoriesController < ApplicationController
+
+  def new
+    @story = Story.new
+    @story.author_id = current_user.id
+    @story.storyable_id = params[:object_id]
+    @story.storyable_type = params[:object_type]
+  end
+
+  def create
+    @story = Story.create(params[:story])
+    if params[:send_email]
+      email_story(@story)
+    end
+  end
+
+  def show
+
+  end
+
+
+  def index
+
+  end
+
+end
