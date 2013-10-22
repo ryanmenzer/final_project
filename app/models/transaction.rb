025332bf050f862f6da664c3b3e20584fe862ce9
cdcueva <cdcueva@sbcglobal.net>
2013-10-21 20:46:18 -0500

    paypal setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -13,4 +13,27 @@ class Transaction < ActiveRecord::Base
   belongs_to :transaction_type
   belongs_to :payer, class_name: "Person"
 
+  
+
+def paypal_url(return_url)
+  values = {
+    :business => 'seller@oliwi.org',
+    :cmd => '_cart',
+    :upload => 1,
+    :return => return_url,
+    :invoice => id
+  }
+  line_items.each_with_index do |item, index|
+    values.merge!({
+      "amount_#{index+1}" => item.unit_price,
+      "item_name_#{index+1}" => item.product.name,
+      "item_number_#{index+1}" => item.id,
+      "quantity_#{index+1}" => item.quantity
+    })
+  end
+  "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+end
+
+
+
 end
