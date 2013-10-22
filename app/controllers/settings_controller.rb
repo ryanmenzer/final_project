c90603fca75f3e86d1c6dc7c69a23ed6cd108573
Eigil Sagafos <eigil@sagafos.no>
2013-10-21 19:42:47 -0500

    Setup setting controller to pass in paypal, mandrill and settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,6 +2,11 @@ class SettingsController < ApplicationController
 
   def index
     @settings = current_tenant.setting
+    @paypal = PaypalSetting.find_or_create_by_tenant_id(current_tenant.id)
+    @mandrill = MandrillSetting.find_or_create_by_tenant_id(current_tenant.id)
+    @header = {title: current_tenant.setting.display_name,
+               subtitle: "Main settings",
+               icon: "icon-cogs" }
   end
 
 end
