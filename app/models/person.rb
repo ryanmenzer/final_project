23b3ba3cf296906c65c3bb1a43d044ab83e78247
Eigil Sagafos <eigil@sagafos.no>
2013-10-21 21:40:05 -0500

    adding has many through relations between people/projects and initiatives

--- app/models/person.rb
+++ app/models/person.rb
@@ -28,6 +28,8 @@ class Person < ActiveRecord::Base
   has_many                :images,    as: :imageable
   has_many                :stories,   as: :storyable
 
+  has_many                :initiatives, through: :receivers
+
   has_and_belongs_to_many :sponsorships
   has_and_belongs_to_many :groups
 
@@ -42,11 +44,11 @@ class Person < ActiveRecord::Base
     self.receivers.each do |receiver|
       receiver.sponsorships.each do |sponsorship|
         sponsorship.sponsors.each do |sponsor|
-            result << sponsor.full_name 
-        end 
-      end 
-    end 
-    result 
+            result << sponsor.full_name
+        end
+      end
+    end
+    result
   end
 
 end
