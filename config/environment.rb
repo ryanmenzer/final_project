af6a2ff6d0721590d1b216af5244cf1307c89c9a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-22 20:10:07 -0500

    Uku adds api connection and function with Algolia search

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,4 @@ require File.expand_path('../application', __FILE__)
 UnusUniRails3::Application.initialize!
 
 # config.gem "carrierwave"
+require 'searchable'
---
+++ lib/searchable.rb
@@ -0,0 +1,54 @@
+module SearchableTenant
+
+  ##################################################################  Params can be changed : https://github.com/algolia/algoliasearch-rails#search-settings
+
+  def search(query)
+    Algolia.client.get(Algolia::Protocol.search_uri(self.id, query, { "attributesToHighlight" => "full_name, email,title",  "hitsPerPage" => 5}))['hits']
+  end
+
+  # build_index people: [:full_name, :email], groups: [:name], initiative: [:title]  <- have to be arrays
+
+  def build_index(params)
+    index = Algolia::Index.new(self.id)
+    
+    params.each do |tablename, getters|
+      klass = Object.const_get(tablename.to_s.classify)
+
+      klass.all.each do |record|
+        object_params = {"tablename" => record.class.table_name,
+                         "url" => "/#{self.class.table_name}/#{self.id}"}
+
+        getters.each{|getter| object_params[getter.to_s] = record.send(getter)}
+
+        index.add_object(object_params)
+      end
+    end
+  end
+
+end
+
+module Searchable
+
+  # include Searchable
+  #
+  #  after_save do
+  #    add_index :full_name, :email        <- as many fields as you like
+  #  end
+  #
+
+  def make_searchable(*attrs)
+    index = Algolia::Index.new(self.tenant.id)
+    object_params = {"id" => self.id, 
+                     "tablename" => self.class.table_name,
+                      "url" => "/#{self.class.table_name}/#{self.id}"}
+
+    attrs.each do |attribute|
+      object_params[attribute.to_s] = self.send(attribute)
+    end
+
+    index.add_object(object_params)
+
+  end
+ 
+end
+
