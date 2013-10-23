6a7f53a010d79e5eed62624905ddcafcc3e13b93
cdcueva <msh345@gmail.com>
2013-10-23 15:26:21 -0500

    add paypal checkout method access to auth rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,18 +1,18 @@
 authorization do 
   role :administrator do 
-    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :editroleform, :editrole]
+    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :paypal_checkout, :editroleform, :editrole]
   end
 
   role :worker do 
-    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :editroleform, :editrole]
+    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :paypal_checkout, :editroleform, :editrole]
   end 
 
   role :guest do 
-    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :editroleform, :editrole]
+    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :paypal_checkout, :editroleform, :editrole]
   end 
 
   role :user do 
-    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :editroleform, :editrole]
+    has_permission_on [:emailresponses, :groups, :homes, :images, :initiatives, :locales, :paypal_payments, :people, :receivers, :settings, :sponsorships, :stories, :transactions, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :addperson, :addgroup, :removeperson, :confirm_payment, :search, :paypal_create, :paypal_checkout, :editroleform, :editrole]
   end 
 end 
 
