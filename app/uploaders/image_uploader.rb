c8d9d209cad87867735db0f584638e3d470bcbb6
Ryan Menzer <ryan.c.menzer@gmail.com>
2013-10-23 16:09:50 -0500

    remove unnecessary photo versions

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,12 +18,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 200, gravity = "Center" ]
   end
 
-  version :small do
-    process :resize_to_limit => [400, 400]
-  end
-
   version :medium do
-    process :resize_to_limit => [800, 800]
+    process :resize_to_limit => [600, 600]
   end
 
   def extension_white_list
