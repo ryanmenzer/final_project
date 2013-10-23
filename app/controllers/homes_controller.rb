529fe60a2e76a21dc7e87a831fb55f959c5064f9
Ryan Menzer <ryan.c.menzer@gmail.com>
2013-10-22 20:27:42 -0500

    recent stories, initiative done

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,10 @@ class HomesController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [ :index ]
 
   def index
-    @initiative = Initiative.first
+    # @initiative = Initiative.first
+    @initiative = Initiative.order("updated_at DESC").limit(1).first
     @stories = Story.limit(10)
+    @images = Image.limit(10)
   end
 
 end
