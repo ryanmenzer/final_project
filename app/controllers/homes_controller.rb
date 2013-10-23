7b3b4f322a63029c14a228d3a408438b7c39a6ff
Eigil Sagafos <eigil@sagafos.no>
2013-10-23 13:06:29 -0500

    Flash errors like growl on every page.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,6 @@ class HomesController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [ :index ]
   filter_access_to :all
   def index
-    # @initiative = Initiative.first
     @initiative = Initiative.order("updated_at DESC").limit(1).first
     @stories = Story.limit(10)
     @images = Image.limit(10)
--- config/navigation.rb
+++ config/navigation.rb
@@ -2,12 +2,12 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     primary.item :dashboard,  t("menu.dashboard"),   root_path, :opts => {:icon => 'icon-dashboard'}
-    primary.item :our_people, t("menu.our_people"), :opts => {:icon => 'icon-user'} do |sub_nav|
-      sub_nav.item :list_all, t("menu.list_all"),        "/people?cat=2", :opts => {:icon => 'icon-list'}
-      sub_nav.item :add_one,  t("menu.add_new"),       new_person_path, :opts => {:icon => 'icon-plus'}
+    primary.item :our_people, t("menu.our_people"),  :opts => {:icon => 'icon-user'} do |sub_nav|
+      sub_nav.item :list_all, t("menu.list_all"),    "/people?cat=2", :opts => {:icon => 'icon-list'}
+      sub_nav.item :add_one,  t("menu.add_new"),     new_person_path, :opts => {:icon => 'icon-plus'}
     end
     primary.item :supporters, t("menu.supporters"), :opts => {:icon => 'icon-trophy'} do |sub_nav|
-      sub_nav.item :list_all, t("menu.list_all"),        "/people?cat=1", :opts => {:icon => 'icon-list'}
+      sub_nav.item :list_all, t("menu.list_all"),    "/people?cat=1", :opts => {:icon => 'icon-list'}
     end
     primary.item :staff,      t("menu.staff"),       "/people?cat=3", :opts => {:icon => 'icon-key'}
     primary.item :initiatives,t("menu.initiatives"), initiatives_path, :opts => {:icon => 'icon-bullhorn'}
