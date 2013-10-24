35d8fa94a917ddaba3bae21f343b82c2842b016f
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-23 19:15:05 -0500

    added middleware to speed up search

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module UnusUniRails3
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.insert_before 0, "SearchMiddleware"
   end
 end
