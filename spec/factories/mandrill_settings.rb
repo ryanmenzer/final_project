caded6236f2f3288aa3a4638b92d4f3742ab3ac9
Maria Fischinger <mmfischinger@gmail.com>
2013-10-24 09:39:24 -0500

    test updates

---
+++ spec/factories/mandrill_settings.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :mandrill_setting do
+  end
+end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe Person do    
-	context "Validations and Associations" do
+describe Person do
+	context "validations and associations" do
     context "saving" do
       before do
         @tenant = Tenant.create!(org_name: "Hello Inc")
@@ -18,7 +18,7 @@ describe Person do
 
       it "should save a person with a category id" do
         expect(@person.save).to eq(true)
-      end  
+      end
 
      # With {}: proc doesn't execute the code / lets rspec catch the exception
   	  it "should not save a person without a category id" do
@@ -26,53 +26,54 @@ describe Person do
   	  end
     end
 
-    it "should belong to category" do
-      person = Person.reflect_on_association(:category)
-      person.macro.should == :belongs_to
-    end
+    context "model validations" do
+      it "should belong to category" do
+        person = Person.reflect_on_association(:category)
+        person.macro.should == :belongs_to
+      end
 
-    it "should belong to user" do
-      person = Person.reflect_on_association(:user)
-      person.macro.should == :belongs_to
-    end
-  
-    # THIS SHOULD BE FIXED AND TEST REMOVED
-    # Has both has_and_belongs_to_many AND has_many
-    it "should have many groups" do
-      pending "something to merge from master??"
-      person = Person.reflect_on_association(:groups)
-      person.macro.should == :has_many
-    end
-  
-    it "should have many stories" do
-      person = Person.reflect_on_association(:stories)
-      person.macro.should == :has_many
-    end
+      it "should belong to user" do
+        person = Person.reflect_on_association(:user)
+        person.macro.should == :belongs_to
+      end
 
-    it "should have many projects" do
-      person = Person.reflect_on_association(:projects)
-      person.macro.should == :has_many
-    end
+      # THIS SHOULD BE FIXED AND TEST REMOVED
+      # Has both has_and_belongs_to_many AND has_many
+      it "should have many groups" do
+        pending "something to merge from master??"
+        person = Person.reflect_on_association(:groups)
+        person.macro.should == :has_many
+      end
 
-    it "should have many initiatives" do
-      person = Person.reflect_on_association(:initiatives)
-      person.macro.should == :has_many
-    end
+      it "should have many stories" do
+        person = Person.reflect_on_association(:stories)
+        person.macro.should == :has_many
+      end
 
-    it "should have many images" do
-      person = Person.reflect_on_association(:images)
-      person.macro.should == :has_many
-    end
+      it "should have many projects" do
+        person = Person.reflect_on_association(:projects)
+        person.macro.should == :has_many
+      end
 
-    it "should have and belong to many sponsorships" do
-      person = Person.reflect_on_association(:sponsorships)
-      person.macro.should == :has_and_belongs_to_many
-    end
+      it "should have many initiatives" do
+        person = Person.reflect_on_association(:initiatives)
+        person.macro.should == :has_many
+      end
 
-    it "should have and belong to many groups" do
-      person = Person.reflect_on_association(:groups)
-      person.macro.should == :has_and_belongs_to_many
-    end
+      it "should have many images" do
+        person = Person.reflect_on_association(:images)
+        person.macro.should == :has_many
+      end
 
+      it "should have and belong to many sponsorships" do
+        person = Person.reflect_on_association(:sponsorships)
+        person.macro.should == :has_and_belongs_to_many
+      end
+
+      it "should have and belong to many groups" do
+        person = Person.reflect_on_association(:groups)
+        person.macro.should == :has_and_belongs_to_many
+      end
+    end
 	end
 end
