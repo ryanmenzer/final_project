5dd3e019e02d11a12e9d4f2d3b60b6fabca72b17
Eigil Sagafos <eigil@sagafos.no>
2013-10-24 12:11:36 -0500

    fix subdomain errors

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,14 +1,14 @@
 SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
-    primary.item :dashboard,  t("menu.dashboard"),   root_path, :opts => {:icon => 'icon-dashboard'}
+    primary.item :dashboard,  t("menu.dashboard"),   dashboard_path, :opts => {:icon => 'icon-dashboard'}
     primary.item :our_people, t("menu.our_people"),  "/people?cat=2", :opts => {:icon => 'icon-user'}
     primary.item :supporters, t("menu.supporters"),  "/people?cat=3", :opts => {:icon => 'icon-trophy'}
     primary.item :staff,      t("menu.staff"),       "/people?cat=1", :opts => {:icon => 'icon-key'}
     primary.item :initiatives,t("menu.initiatives"), initiatives_path, :opts => {:icon => 'icon-bullhorn'}
     primary.item :sponsorships,t("menu.sponsorships"), sponsorships_path, :opts => {:icon => 'icon-heart'}
     primary.item :groups,     t("menu.groups"),      groups_path, :opts => {:icon => 'icon-group'}
-    primary.item :other,      t("menu.Settings"),    settings_path, :opts => {:icon => 'icon-cog'}
+    primary.item :other,      t("menu.settings"),    settings_path, :opts => {:icon => 'icon-cog'}
   end
 
 end
