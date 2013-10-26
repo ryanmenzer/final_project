6a110ba9712334a997f2eaddc9206effbdda91cf
Eigil Sagafos <eigil@sagafos.no>
2013-10-26 18:35:19 -0500

    Made all searching update and destroy in algolia

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,12 +6,21 @@ class Group < ActiveRecord::Base
 
   include Searchable
 
-  after_save do
-    make_searchable :name
+  after_create do
+    make_searchable :name, :email
+  end
+
+  before_destroy do
+    destroy_search
+  end
+
+  before_update do
+    update_search :name, :email
   end
 
   attr_accessible :name,
-                  :manager_id
+                  :manager_id,
+                  :algolia_id
 
   has_and_belongs_to_many :members, class_name: 'Person'
   belongs_to :manager, class_name: 'Person'
